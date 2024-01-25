1eb6423571aad3258927eeba1505d815eab5fa4f
ulsandonghun <donghoon141@naver.com>
2024-01-25 22:48:44 +0900

    [set] : 첫 세팅 완료

---
+++ src/test/java/com/cona/KUsukKusuk/KuSukKuSukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cona.KUsukKusuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuSukKuSukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
