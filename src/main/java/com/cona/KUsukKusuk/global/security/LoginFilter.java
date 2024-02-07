938ab0641b452c93d43065bc6ea7cc667ac46937
ulsandonghun <donghoon141@naver.com>
2024-02-08 01:43:10 +0900

    [add] : 커스텀 UsernamePasswordAuthenticationFilter 구현

---
+++ src/main/java/com/cona/KUsukKusuk/global/security/LoginFilter.java
@@ -0,0 +1,42 @@
+package com.cona.KUsukKusuk.global.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public LoginFilter(AuthenticationManager authenticationManager) {
+
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        //클라이언트 요청에서 username, password 추출
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
+
+        return authenticationManager.authenticate(authToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+
+    }
+}
--- src/main/java/com/cona/KUsukKusuk/user/repository/UserRepository.java
+++ src/main/java/com/cona/KUsukKusuk/user/repository/UserRepository.java
@@ -1,8 +1,14 @@
 package com.cona.KUsukKusuk.user.repository;
 
 import com.cona.KUsukKusuk.user.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUserId(String userid);
+
+    boolean existsByNickname(String nickname);
+
+    boolean existsByEmail(String email);
 
 }
