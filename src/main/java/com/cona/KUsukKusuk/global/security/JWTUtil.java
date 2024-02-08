e24e231742afc8baa3489492fe1337119d54c377
ulsandonghun <donghoon141@naver.com>
2024-02-09 05:46:00 +0900

    [feat] : 리프레시 토큰 발급시 redis에 아이디와 저장 기능 구현

--- src/main/java/com/cona/KUsukKusuk/global/security/JWTUtil.java
+++ src/main/java/com/cona/KUsukKusuk/global/security/JWTUtil.java
@@ -3,14 +3,23 @@ package com.cona.KUsukKusuk.global.security;
 import io.jsonwebtoken.Jwts;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JWTUtil {
 
+    @Autowired
+    private  RedisTemplate<String, String> redisTemplate;
     private SecretKey secretKey;
 
     public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
@@ -46,13 +55,26 @@ public class JWTUtil {
     }
     public String createRefreshToken(String userid, String password, Long expiredMs) {
 
-        return Jwts.builder()
+
+        String refreshToken = Jwts.builder()
                 .claim("userid", userid)
                 .claim("password", password)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + expiredMs))
                 .signWith(secretKey)
                 .compact();
+
+        // redis에 저장
+        redisTemplate.opsForValue().set(
+                userid,
+                refreshToken,
+                expiredMs,
+                TimeUnit.MILLISECONDS
+        );
+
+
+
+        return refreshToken;
     }
 
 }
