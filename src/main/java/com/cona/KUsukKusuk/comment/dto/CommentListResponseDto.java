a51f9de4c4ace3630375e1a3794de483ad654e72
ulsandonghun <donghoon141@naver.com>
2024-04-04 10:20:30 +0900

    [fix] : 사용자가 쓴 댓글 페이지네이션 형식 통일

---
+++ src/main/java/com/cona/KUsukKusuk/comment/dto/CommentListResponseDto.java
@@ -0,0 +1,44 @@
+package com.cona.KUsukKusuk.comment.dto;
+
+import com.cona.KUsukKusuk.comment.domain.Comment;
+import com.cona.KUsukKusuk.spot.domain.Spot;
+import com.cona.KUsukKusuk.user.dto.BoomarkLikeResponseDto;
+import com.cona.KUsukKusuk.user.dto.PageInfo;
+import java.time.LocalDateTime;
+import lombok.Builder;
+
+@Builder
+public record CommentListResponseDto(
+        String spotName,
+        Long spotId,
+        String review,
+        LocalDateTime createDate,
+
+        String author,
+
+
+        String spotImageurl,
+        long totalElements,
+        int page,
+        int size,
+        int totalPages
+) {
+
+    public static CommentListResponseDto of (Comment comment, PageInfo pageInfo){
+        return CommentListResponseDto.builder()
+                .spotName(comment.getSpot().getSpotName())
+                .spotImageurl(comment.getSpot().getImageUrls().get(0))
+                .createDate(comment.getCreatedDate())
+                .spotId(comment.getId())
+                .review(comment.getSpot().getReview())
+                .author(comment.getUser().getNickname())
+
+                .totalElements(pageInfo.getTotalElements())
+                .page(pageInfo.getPage())
+                .size(pageInfo.getSize())
+                .totalPages(pageInfo.getTotalPages())
+                .build();
+    }
+
+
+}
--- src/main/java/com/cona/KUsukKusuk/comment/repository/CommentRepository.java
+++ src/main/java/com/cona/KUsukKusuk/comment/repository/CommentRepository.java
@@ -2,9 +2,13 @@ package com.cona.KUsukKusuk.comment.repository;
 
 import com.cona.KUsukKusuk.comment.domain.Comment;
 import com.cona.KUsukKusuk.user.domain.User;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     void deleteAllByUser(User user);
+
+    List<Comment> findByUser(User user);
 
 }
